3ecf17b518dabeba4ef001be328089498974131a
stracciatella27 <schoko.bombe44@gmail.com>
2023-05-08 20:32:57 +0200

    add index to prettierignore and lintignore

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,12 @@ import $ from 'jquery';
  * @returns {JQuery<HTMLElement>} A list of elements that have text inside of them
  */
 function extractElementsToTranslate() {
-    const elements = $('body *:visible').filter((_, element) => {
-        const clone = $(element).clone();
-        clone.children().remove();
-        return clone.text().trim().length > 0;
-    });
-    return elements;
+	const elements = $('body *:visible').filter((_, element) => {
+		const clone = $(element).clone();
+		clone.children().remove();
+		return clone.text().trim().length > 0;
+	});
+	return elements;
 }
 
 const elements = extractElementsToTranslate();
@@ -18,22 +18,22 @@ const elements = extractElementsToTranslate();
 let charCount = 0;
 
 elements.each((_, element) => {
-    const clone = $(element).clone();
-    const children = clone.children().clone();
+	const clone = $(element).clone();
+	const children = clone.children().clone();
 
-    // Remove all children
-    clone.children().remove();
+	// Remove all children
+	clone.children().remove();
 
-    // overwrite text in clone 
-    // clone.text('Ass');
+	// overwrite text in clone
+	// clone.text('Ass');
 
-    charCount += clone.text().length
+	charCount += clone.text().length;
 
-    // append children back to clone
-    clone.append(children);
+	// append children back to clone
+	clone.append(children);
 
-    // replace original element with clone
-    $(element).replaceWith(clone);
+	// replace original element with clone
+	$(element).replaceWith(clone);
 });
 
 console.log(charCount);
