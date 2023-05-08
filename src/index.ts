177db17daf13a754dca384f3ea0a378b983c5140
stracciatella27 <schoko.bombe44@gmail.com>
2023-05-08 20:29:36 +0200

    add text extraction update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,39 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import $ from 'jquery';
+
+/**
+ * Extracts all the elements visible in the body of the page that have text directly in them
+ * @returns {JQuery<HTMLElement>} A list of elements that have text inside of them
+ */
+function extractElementsToTranslate() {
+    const elements = $('body *:visible').filter((_, element) => {
+        const clone = $(element).clone();
+        clone.children().remove();
+        return clone.text().trim().length > 0;
+    });
+    return elements;
+}
+
+const elements = extractElementsToTranslate();
+
+let charCount = 0;
+
+elements.each((_, element) => {
+    const clone = $(element).clone();
+    const children = clone.children().clone();
+
+    // Remove all children
+    clone.children().remove();
+
+    // overwrite text in clone 
+    // clone.text('Ass');
+
+    charCount += clone.text().length
+
+    // append children back to clone
+    clone.append(children);
+
+    // replace original element with clone
+    $(element).replaceWith(clone);
+});
+
+console.log(charCount);
